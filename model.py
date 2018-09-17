e3f8ebba2ff208cbb56a790e5ba8706e2ad8780f
z <kukharev.av@gmail.com>
2018-09-17 09:26:58 +0300

    1

--- model.py
+++ model.py
@@ -1,14 +1,23 @@
 import os.path
 
+import network
+
 if os.path.exists('.notebook'):
 	#data_dir = '../data'
 	data_dir = '../separated'
+
 	module = network.conv_network_224
 	SHAPE = 224, 224, 3
+
+	bottleneck_tensor_size =  588
+	BATCH_SIZE = 2
+	DISPLAY_INTERVAL, NUM_ITERS = 1, 500	
+
 else:
 	#data_dir = '/home/chichivica/Data/Datasets/Scales/data'
 	#data_dir = '/home/chichivica/Data/Datasets/Scales/separated'
 	data_dir = '/home/chichivica/Data/Datasets/Scales/separated_cropped/'
+
 	import tensorflow_hub as hub
 
 	model_number = 3
@@ -27,3 +36,8 @@ else:
 	else:
 		raise Exception('Bad n_model')
 		# https://tfhub.dev/google/imagenet/resnet_v2_152/feature_vector/1
+
+	bottleneck_tensor_size =  1536
+	#bottleneck_tensor_size =  1001
+	BATCH_SIZE = 10
+	DISPLAY_INTERVAL, NUM_ITERS = 100, 20*1000*1000		
