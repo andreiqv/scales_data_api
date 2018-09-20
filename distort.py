b9697763353dca18c343a8cc5e49e9e5e1aad1c8
z <kukharev.av@gmail.com>
2018-09-20 09:22:22 +0300

    1

--- distort.py
+++ distort.py
@@ -10,10 +10,14 @@ def auпgment_dataset_no_labels(dataset, mult=1):
 	def _random_distord(images):
 
 		images = tf.image.random_flip_left_right(images)
-		images = tf.image.random_hue(images, max_delta=0.05)
-		images = tf.image.random_contrast(images, lower=0.3, upper=1.8)
-		images = tf.image.random_brightness(images, max_delta=0.3)
-		images = tf.image.random_saturation(images, lower=0.0, upper=2.0)
+		#images = tf.image.random_hue(images, max_delta=0.05)
+		#images = tf.image.random_contrast(images, lower=0.3, upper=1.8)
+		#images = tf.image.random_brightness(images, max_delta=0.3)
+		#images = tf.image.random_saturation(images, lower=0.0, upper=2.0)
+		images = tf.image.random_hue(images, max_delta=0.02)
+		images = tf.image.random_contrast(images, lower=0.5, upper=1.5)
+		images = tf.image.random_brightness(images, max_delta=0.2)
+		images = tf.image.random_saturation(images, lower=0.5, upper=1.5)
 		images = tf.minimum(images, 255.0)
 		images = tf.maximum(images, 0.0)
 		return images
