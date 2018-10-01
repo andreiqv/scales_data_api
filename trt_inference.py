4f787b5460e010d962e3cae902f9c278fa716469
z <kukharev.av@gmail.com>
2018-10-01 15:09:56 +0300

    1

--- trt_inference.py
+++ trt_inference.py
@@ -19,26 +19,25 @@ CLASSES = [str(i) for i in range(NUM_CLASSES)] # ADJUST
 use_hub_model = False
 
 if use_hub_model:
-	FROZEN_FPATH = '/home/andrei/Data/Datasets/Scales/pb/output_graph.pb'
+	#FROZEN_FPATH = '/home/andrei/Data/Datasets/Scales/pb/output_graph.pb'
 	ENGINE_FPATH = '/home/andrei/Data/Datasets/Scales/pb/hub_model_engine.plan'
-	INPUT_NODE = 'Placeholder-x'
-	OUTPUT_NODE = 'final_result'
+	#INPUT_NODE = 'Placeholder-x'
+	#OUTPUT_NODE = 'final_result'
 	INPUT_SIZE = [3, 299, 299]
 
 else:
 	#FROZEN_FPATH = '/root/tmp/saved_model_inception_resnet.pb'
 	#ENGINE_FPATH = '/root/tmp/engine.plan'
-	FROZEN_FPATH = 'saved_model_full.pb'
+	#FROZEN_FPATH = 'saved_model_full.pb'
 	ENGINE_FPATH = 'saved_model_full.plan'
-	INPUT_NODE = 'Placeholder-x'
-	OUTPUT_NODE = 'sigmoid_out'
+	#INPUT_NODE = 'Placeholder-x'
+	#OUTPUT_NODE = 'sigmoid_out'
 	INPUT_SIZE = [3, 299, 299]
 
-MAX_BATCH_SIZE = 1
-MAX_WORKSPACE = 1 << 20
+#MAX_BATCH_SIZE = 1
+#MAX_WORKSPACE = 1 << 20
 
 CROP_SIZE = tuple(INPUT_SIZE[1:])
-
 
 
 def prepare_image(image_path):
